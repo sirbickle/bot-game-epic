beedf3c17d8374bab45bb63e2f55b98a27498055
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2024-03-08 13:31:18 -0300

    teste

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if confirm == True:
     time.sleep(5)
     try:
         pyautogui.click('gratis.png')
-        time.sleep(2)
+        time.sleep(5)
         confirm = Biblioteca.ExistsOnScreen('NaBiblioteca.png')
         time.sleep(2)
         if confirm == True:
@@ -25,9 +25,9 @@ if confirm == True:
             Biblioteca.SendMailPossui()
             ###os.system("shutdown /s /t 10")
         else:
-            pyautogui.click('obter.png')
+            pyautogui.click(1600, 700)
             time.sleep(5)
-            pyautogui.click('fazer_pedido.png')
+            pyautogui.click(1450,700)
             time.sleep(5)
             pyautogui.click('ver_na_biblioteca.png')
             time.sleep(5)
@@ -39,30 +39,48 @@ if confirm == True:
         Biblioteca.SendMailErro()
         ###os.system("shutdown /s /t 10")
 else:
-    pyautogui.moveTo(1000,500)
+    pyautogui.moveTo(1500,800)
     pyautogui.scroll(-1700)
     time.sleep(5)
     try:
         pyautogui.click('gratis.png')
         time.sleep(5)
-        confirm = Biblioteca.ExistsOnScreen('NaBiblioteca.png')
-        time.sleep(2)
-        if confirm == True:
-            pyautogui.hotkey('alt', 'f4')
-            Biblioteca.SendMailPossui()
-            ###os.system("shutdown /s /t 10")
+        confirm2 = Biblioteca.ExistsOnScreen("continuar.png")
+        if confirm2 == True:
+            confirm = Biblioteca.ExistsOnScreen('NaBiblioteca.png')
+            time.sleep(2)
+            if confirm == True:
+                pyautogui.hotkey('alt', 'f4')
+                Biblioteca.SendMailPossui()
+                ###os.system("shutdown /s /t 10")
+            else:
+                pyautogui.click(1600, 700)
+                time.sleep(15)
+                pyautogui.click(1450,700)
+                time.sleep(15)
+                pyautogui.click('ver_na_biblioteca.png')
+                time.sleep(15)
+                pyautogui.hotkey('alt', 'f4')
+                Biblioteca.SendMailPossui()
+                ###os.system("shutdown /s /t 10")
         else:
-            pyautogui.click('obter.png')
-            time.sleep(5)
-            pyautogui.click('fazer_pedido.png')
-            time.sleep(5)
-            pyautogui.click('ver_na_biblioteca.png')
-            time.sleep(5)
-            pyautogui.hotkey('alt', 'f4')
-            Biblioteca.SendMailPossui()
-            ###os.system("shutdown /s /t 10")
+            confirm = Biblioteca.ExistsOnScreen('NaBiblioteca.png')
+            time.sleep(2)
+            if confirm == True:
+                pyautogui.hotkey('alt', 'f4')
+                Biblioteca.SendMailPossui()
+                ###os.system("shutdown /s /t 10")
+            else:
+                pyautogui.click(1600, 700)
+                time.sleep(15)
+                pyautogui.click(1450,700)
+                time.sleep(15)
+                pyautogui.click('ver_na_biblioteca.png')
+                time.sleep(15)
+                pyautogui.hotkey('alt', 'f4')
+                Biblioteca.SendMailPossui()
+                ###os.system("shutdown /s /t 10")
     except pyautogui.ImageNotFoundException:
         pyautogui.hotkey('alt', 'f4')
         Biblioteca.SendMailErro()
         ###os.system("shutdown /s /t 10")
-
